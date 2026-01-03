7a625c989216383c54af2522131b7a9df7e2aaa7
vishnuraghu021-bit <vishnuraghu021@gmail.com>
2026-01-03 08:57:27 +0530

    Add main function to calculate scores statistics

---
+++ arr.py
@@ -0,0 +1,11 @@
+def main():
+  scores=[76,82,85,72,92]
+  Total=sum(scores)
+  average=Total/len(scores)
+  print("===main/master branch output===")
+  print(f"Scores:{scores}")
+  print(f"Sum:{Total}")
+  print(f"Average:{average}")
+if __name__=="__main__":
+  main()
+  
